fd6a0f3dd1e51f5fcc0fc830ed0026c552e3e31d
camilomontana <47892397+camilomontana@users.noreply.github.com>
2023-06-26 20:32:32 -0500

    adicion de consulta por especilidad

--- app/src/main/java/com/example/citas_medicas/Consult.java
+++ app/src/main/java/com/example/citas_medicas/Consult.java
@@ -3,26 +3,33 @@ package com.example.citas_medicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class Consult extends AppCompatActivity {
     private ListView listUsers;
     private Button btnConsult;
+    private Button btnConsultID;
+    private Spinner spinnerID;
     DataBase dataBase = new DataBase();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consult);
+
+        spinnerID = findViewById(R.id.spinnerID);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerID, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerID.setAdapter(adapter);
+
         initialize();
         btnConsult.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +37,7 @@ public class Consult extends AppCompatActivity {
                 //Toast.makeText(Consult.this, "Consulta",Toast.LENGTH_LONG).show();
                // Toast.makeText(Consult.this, dataBase.getcitas().getNombre(),Toast.LENGTH_LONG).show();
                //        android.R.layout.simple_list_item_1);
+                Log.i(dataBase.getCitas().toString(), "Tengo los datos " + dataBase.getCitas().size());
 
                 String[] str = new String[dataBase.getCitas().size()];
 
@@ -43,9 +51,37 @@ public class Consult extends AppCompatActivity {
 
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(Consult.this, android.R.layout.simple_list_item_1, str);
                 listUsers.setAdapter(adapter);
-                //listUsers.setAdapter(Arrays.toString(adapter));
+            }
+        });
 
+        btnConsultID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                ArrayList<Citas> newListCitas = new ArrayList<>();
+
+                //Log.i(spinnerID.getSelectedItem().toString(), "Esto seleccione");
+
+                for (Citas elements : dataBase.getCitas()) {
+                    if (elements.getEspecialidad().equals(spinnerID.getSelectedItem().toString())) {
+                        newListCitas.add(elements);
+                    }
+                }
+
+                //Log.i(dataBase.getCitas().toString(), "Tengo los datos old " + dataBase.getCitas().size());
+                //Log.i(newListCitas.toString(), "Tengo los datos new " + newListCitas.size());
+
+                String[] strFilter = new String[newListCitas.size()];
+
+                for (int i = 0; i < newListCitas.size(); i++) {
+                    strFilter[i] = (newListCitas.get(i).getNombre()+" "+
+                            newListCitas.get(i).getEspecialidad()+" "+
+                            newListCitas.get(i).getMedico()+" "+
+                            newListCitas.get(i).getFecha());
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(Consult.this, android.R.layout.simple_list_item_1, strFilter);
+                listUsers.setAdapter(adapter);
             }
         });
 
@@ -54,5 +90,6 @@ public class Consult extends AppCompatActivity {
     private void initialize(){
         listUsers = (ListView) findViewById(R.id.listUsers);
         btnConsult = (Button) findViewById(R.id.btnConsult);
+        btnConsultID = (Button) findViewById(R.id.btnConsultID);
     }
 }
--- app/src/main/java/com/example/citas_medicas/Register.java
+++ app/src/main/java/com/example/citas_medicas/Register.java
@@ -2,31 +2,46 @@ package com.example.citas_medicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
     private EditText txtName;
-    private EditText txtId;
-    private EditText txtEmail;
+    private Spinner spinnerRID;
+    private Spinner spnMedico;
     private EditText txtBirthday;
     private Button btnRegister;
 
     DataBase dataBase = new DataBase();
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        spinnerRID = findViewById(R.id.spinnerRID);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerID, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerRID.setAdapter(adapter);
+
+        spnMedico = findViewById(R.id.spnMedico);
+        ArrayAdapter<CharSequence> adapterTwo = ArrayAdapter.createFromResource(this, R.array.spnMedico, android.R.layout.simple_spinner_item);
+        adapterTwo.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spnMedico.setAdapter(adapterTwo);
+
         initialize();
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Toast.makeText(Register.this, txtName.getText(),Toast.LENGTH_LONG).show();
-                dataBase.addcitas(txtName.getText().toString(),txtId.getText().toString(),txtEmail.getText().toString(),txtBirthday.getText().toString());
+                dataBase.addcitas(txtName.getText().toString(), spinnerRID.getSelectedItem().toString(), spnMedico.getSelectedItem().toString(), txtBirthday.getText().toString());
                 //dataBase.addcitas("A","N","C", "D");
                 Toast.makeText(Register.this, "Se ha registrado la cita",Toast.LENGTH_LONG).show();
             }
@@ -35,8 +50,6 @@ public class Register extends AppCompatActivity {
     private void initialize(){
 
         txtName = (EditText) findViewById(R.id.txtName);
-        txtId = (EditText) findViewById(R.id.txtEspecialidad);
-        txtEmail = (EditText) findViewById(R.id.txtMedico);
         txtBirthday = (EditText) findViewById(R.id.txtBirthday);
         btnRegister = (Button) findViewById(R.id.btnRegister);
     }
